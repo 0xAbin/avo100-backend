138c90ee817a0006ddf27f463aeb85f18e323cd5
0xabin <abinbvinod@gmail.com>
2025-01-22 00:15:38 +0530

    Fetched top 100 coin Symbol

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-
 mod utils;
-use crate::utils::get_assets::get_assets; 
+use utils::get_assets::get_assets;
 
 fn main() {
-    get_assets();  
+    match get_assets() {
+        Ok(rank_symbol_pairs) => {
+            for (rank, symbol) in rank_symbol_pairs {
+                println!("{}: {}", rank, symbol);
+            }
+        }
+        Err(err) => {
+            eprintln!("Error: {:?}", err);
+        }
+    }
 }
--- src/utils/get_assets.rs
+++ src/utils/get_assets.rs
@@ -1,35 +1,53 @@
 use reqwest::blocking::get;
-use reqwest::Error;
-use serde::Deserialize;
 use serde_json::Value;
 use std::env;
 use dotenv::dotenv;
+use std::collections::HashSet;
 
-#[derive(Debug)]
-pub struct Asset {
-    pub symbol: String,
-}
+pub fn get_assets() -> Result<Vec<(usize, String)>, Box<dyn std::error::Error>> {
 
-#[derive(Deserialize)]
-struct Resonse {
-    values : Vec<Vec<serde_json::Value>>,
-}
-
-// pub fn get_assets() -> Result<Vec<Asset>, Error> {
-
-//     dotenv().ok();
-    
-//     let url = env::var("URL").expect("URL must be set");
-
-//     println!("URL: {}", url);
-  
-// }
-
-pub fn get_assets() {
-    
     dotenv().ok();
-
     let url = env::var("URL").expect("URL must be set");
 
-    println!("URL: {}", url);
+
+    let response = get(&url)?.text()?;
+    let data: Value = serde_json::from_str(&response)?;
+
+    // Need to work on this 
+    let fields = data["fields"].as_array().expect("Expected fields array");
+    let rank_index = fields
+        .iter()
+        .position(|field| field.as_str() == Some("rank"))
+        .expect("Rank field not found");
+    let symbol_index = fields
+        .iter()
+        .position(|field| field.as_str() == Some("symbol"))
+        .expect("Symbol field not found");
+
+    let values = data["values"].as_array().expect("Expected values array");
+    let excluded_symbols: HashSet<&str> = ["USDT", "USDC"].iter().cloned().collect();
+
+    let mut rank_symbol_pairs: Vec<(i64, String)> = values
+        .iter()
+        .filter_map(|entry| {
+            let rank = entry[rank_index].as_i64()?;
+            let symbol = entry[symbol_index].as_str()?;
+            if rank > 0 && !excluded_symbols.contains(symbol) {
+                Some((rank, symbol.to_string()))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    rank_symbol_pairs.sort_by_key(|k| k.0);
+
+    let top_100: Vec<(usize, String)> = rank_symbol_pairs
+        .into_iter()
+        .take(100)
+        .enumerate()
+        .map(|(i, (_, symbol))| (i + 1, symbol)) 
+        .collect();
+
+    Ok(top_100)
 }
